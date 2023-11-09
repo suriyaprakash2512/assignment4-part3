8c198fa8a6d454279d498b371cc08c73f9dd8536
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2023-11-09 15:19:43 +0530

    assignment4-part3

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_part3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintFibonacciSequence(10);
+        }
+
+        static void PrintFibonacciSequence(int count)
+        {
+            int i1 = 0, i2 = 1, next;
+
+            Console.WriteLine("The first 10 numbers in the Fibonacci sequence:");
+            Console.Write(i1 + " " + i2 + " ");
+
+            for (int i = 2; i < count; i++)
+            {
+                next = i1 + i2;
+                Console.Write(next + " ");
+                i1 = i2;
+                i2 = next;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
